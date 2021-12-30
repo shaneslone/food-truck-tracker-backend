d1159bb52a8f7c434a4664bcd769f221019d5124
Shane Slone <slone.shane@gmail.com>
2021-12-29 23:45:13 -0500

    added class for menu item service

---
+++ src/main/java/com/foodtrucktracker/foundation/services/MenuItemServiceImpl.java
@@ -0,0 +1,32 @@
+package com.foodtrucktracker.foundation.services;
+
+import com.foodtrucktracker.foundation.models.MenuItem;
+
+import java.util.List;
+
+public class MenuItemServiceImpl implements MenuItemService{
+    @Override
+    public List<MenuItem> findAll() {
+        return null;
+    }
+
+    @Override
+    public MenuItem findMenuItemById(long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public MenuItem save(MenuItem menuItem) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
