e13979c27216bca47fac0a854153d9cda0b0678d
Shane Slone <slone.shane@gmail.com>
2022-01-01 17:55:05 -0500

    fixed swagger documentation

--- src/main/java/com/foodtrucktracker/foundation/config/Swagger2Config.java
+++ src/main/java/com/foodtrucktracker/foundation/config/Swagger2Config.java
@@ -32,7 +32,7 @@ public class Swagger2Config
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
             .apis(RequestHandlerSelectors
-                .basePackage("com.lambdaschool.usermodel"))
+                .basePackage("com.foodtrucktracker.foundation"))
             .paths(PathSelectors.regex("/.*"))
             .build()
             .apiInfo(apiEndPointsInfo());
