96bc6d7bb47c3f7b07ecbaf3e934cdb18d2a1a24
Shane Slone <slone.shane@gmail.com>
2022-01-05 17:15:28 -0500

    added menu item controller

---
+++ src/main/java/com/foodtrucktracker/foundation/controllers/MenuItemController.java
@@ -0,0 +1,61 @@
+package com.foodtrucktracker.foundation.controllers;
+
+import com.foodtrucktracker.foundation.models.MenuItem;
+import com.foodtrucktracker.foundation.services.MenuItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/menuitem")
+public class MenuItemController {
+    @Autowired
+    private MenuItemService menuItemService;
+
+    @GetMapping(value = "/menuitems", produces = "application/json")
+    public ResponseEntity<?> listAllMenuItems(){
+        List<MenuItem> menuItems = menuItemService.findAll();
+        return new ResponseEntity<>(menuItems, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/menuitem/{menuItemId}", produces = "application/json")
+    public ResponseEntity<?> getMenuItemById(@PathVariable long menuItemId){
+        MenuItem menuItem = menuItemService.findMenuItemById(menuItemId);
+        return new ResponseEntity<>(menuItem, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/truck", consumes = "application/json")
+    public ResponseEntity<?> addMenuItem(@Valid @RequestBody MenuItem newMenuItem){
+        newMenuItem.setMenuId(0);
+        newMenuItem = menuItemService.save(newMenuItem);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newMenuItemURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{menuItemId}")
+                .buildAndExpand(newMenuItem.getMenuId())
+                .toUri();
+        responseHeaders.setLocation(newMenuItemURI);
+        return new ResponseEntity<>(newMenuItem, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/menuitem/{menuItemId}", consumes = "application/json")
+    public ResponseEntity<?> updateMenuItem(@Valid @RequestBody MenuItem updateMenuItem, @PathVariable long menuItemId){
+        updateMenuItem.setMenuId(menuItemId);
+        updateMenuItem = menuItemService.save(updateMenuItem);
+
+        return new ResponseEntity<>(updateMenuItem, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/menuitem/{menuItemId}")
+    public ResponseEntity<?> deleteMenuItemById(@PathVariable long menuItemId){
+        menuItemService.delete(menuItemId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
