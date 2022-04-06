e6f75166dc546c8d2cbc773c03c4b5076f3abdab
Shane Slone <slone.shane@gmail.com>
2022-04-06 17:26:09 -0400

    changed datasource initialization

--- src/main/java/com/foodtrucktracker/foundation/models/Truck.java
+++ src/main/java/com/foodtrucktracker/foundation/models/Truck.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
